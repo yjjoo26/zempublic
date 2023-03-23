c76b595913a6a18fd86dbac426f3d776890803af
dev.lyj <dev.lyj@zempublic.co.kr>
2023-03-23 11:55:31 +0900

    antd 적용

---
+++ my-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from 'antd/es/button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Primary</Button>
+      <Button>Default</Button>
+      <Button type="dashed">Dashed</Button>
+      <Button type="danger">Danger</Button>
+      <Button type="link">Link</Button>
+    </div>
+  );
+}
+
+export default App;
